6a78fbe313119255b058d8a460fa70f5fc7225dc
Svetagim <svetagimpelson@se.shenkar.ac.il>
2019-05-16 18:14:33 +0300

    components

--- App.js
+++ App.js
@@ -2,12 +2,16 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import GeolocationExample from './components/GeoLocationExample'
 import Map from './components/Map'
+import Gyroscope from './components/Gyroscope'
+import Gestures from './components/Gestures'
 import MapView, { MAP_TYPES } from 'react-native-maps';
 
 export default class App extends React.Component {
   render() {
     return (
-      <Map/>
+      // <Map/>
+      // <Gestures/>
+      <Gyroscope/>
     );
   }
 }
