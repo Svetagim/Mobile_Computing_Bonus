ee066ab90159246f03b36fe583cdda40691b983a
Svetagim <svetagimpelson@se.shenkar.ac.il>
2019-05-23 13:14:44 +0300

    app

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 import GeolocationExample from './components/GeoLocationExample'
 import Map from './components/Map'
 import Gyroscope from './components/Gyroscope'
@@ -10,7 +10,17 @@ import Accel from './components/Accel'
 import LongPress from './components/LongPress'
 import MapView, { MAP_TYPES } from 'react-native-maps';
 
-export default class App extends React.Component {
+export default class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      currentComponent: "Accel"
+    }
+  }
+
+  // renderComponenten(){
+
+  // }
   render() {
     return (
       // <Map/>
@@ -19,16 +29,66 @@ export default class App extends React.Component {
       // <LongPress/>
       // <Gyroscope/>
       // <Sphere/>
-      <Accel/>
-    );
+      // <Accel/>
+      <View style={styles.container}>
+        <View style={styles.buttonContainer}>
+            <TouchableOpacity style={styles.button}>
+              <Text>Geo-Location</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={[styles.button, styles.Button]}>
+            {/* onPress={()=>{this.setState(()=>{currentComponent:"Gyroscope"})}}> */}
+              <Text>Gyroscope</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.button}>
+              <Text>Accelerometer</Text>
+            </TouchableOpacity>
+          </View>
+          <View style={styles.buttonContainer}>
+          <TouchableOpacity style={styles.button}>
+            <Text>drag</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={[styles.button, styles.Button]}>
+            <Text>swipe</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.button}>
+            <Text>long press</Text>
+          </TouchableOpacity>
+        </View>
+       <Text>{this.state.currentComponent}</Text>
+      </View>
+    )};
   }
-}
+
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
+    marginTop: 40
   },
+  buttonContainer: {
+    flexDirection: 'row',
+    alignItems: 'stretch',
+    // marginTop: 15,
+  },
+  button: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#eee',
+    padding: 10,
+    // borderLeftWidth: 1,
+    // borderRightWidth: 1,
+    // borderTopWidth: 1,
+    // borderBottomWidth: 1,
+  },
+  middleButton: {
+    borderLeftWidth: 1,
+    borderRightWidth: 1,
+    borderTopWidth: 1,
+    borderBottomWidth: 1,
+    borderColor: '#ccc',
+  }
 });
--- components/Accel.js
+++ components/Accel.js
@@ -1,77 +1,112 @@
 import React, { Component } from 'react';
-import {
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
-import { Accelerometer } from "react-native-sensors";
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { Accelerometer } from 'expo';
 
-const Value = ({name, value}) => (
-  <View style={styles.valueContainer}>
-    <Text style={styles.valueName}>{name}:</Text>
-    <Text style={styles.valueValue}>{new String(value).substr(0, 8)}</Text>
-  </View>
-)
+export default class Accel extends Component {
+  state = {
+    accelerometerData: {},
+  };
 
-export default class App extends Component {
-  constructor(props) {
-    super(props);
-
-    new Accelerometer({
-      updateInterval: 400 // defaults to 100ms
-    })
-      .then(observable => {
-        observable.subscribe(({x,y,z}) => this.setState({x,y,z}));
-      })
-      .catch(error => {
-        console.log("The sensor is not available");
-      });
-      
-    this.state = {x: 0, y: 0, z: 0};
+  componentDidMount() {
+    this._toggle();
   }
-  
+
+  componentWillUnmount() {
+    this._unsubscribe();
+  }
+
+  _toggle = () => {
+    if (this._subscription) {
+      this._unsubscribe();
+    } else {
+      this._subscribe();
+    }
+  };
+
+  _slow = () => {
+    Accelerometer.setUpdateInterval(1000); 
+  };
+
+  _fast = () => {
+    Accelerometer.setUpdateInterval(
+      16
+    ); 
+  };
+
+  _subscribe = () => {
+    this._subscription = Accelerometer.addListener(
+      accelerometerData => {
+        this.setState({ accelerometerData });
+      }
+    ); 
+  };
+
+  _unsubscribe = () => {
+    this._subscription && this._subscription.remove(); 
+    this._subscription = null;
+  };
+
   render() {
+    let {
+      x,
+      y,
+      z,
+    } = this.state.accelerometerData; 
+
     return (
-      <View style={styles.container}>
-        <Text style={styles.headline}>
-          Accelerometer values
+      <View style={styles.sensor}>
+        <Text>Accelerometer:</Text>
+        <Text>
+          x: {round(x)} y: {round(y)} z: {round(z)}
         </Text>
-        <Value name="x" value={this.state.x} />
-        <Value name="y" value={this.state.y} />
-        <Value name="z" value={this.state.z} />
+
+        <View style={styles.buttonContainer}>
+          <TouchableOpacity onPress={this._toggle} style={styles.button}>
+            <Text>Toggle</Text>
+          </TouchableOpacity>
+          <TouchableOpacity onPress={this._slow} style={[styles.button, styles.middleButton]}>
+            <Text>Slow</Text>
+          </TouchableOpacity>
+          <TouchableOpacity onPress={this._fast} style={styles.button}>
+            <Text>Fast</Text>
+          </TouchableOpacity>
+        </View>
       </View>
     );
   }
 }
 
+function round(n) {
+  if (!n) {
+    return 0;
+  }
+
+  return Math.floor(n * 100) / 100;
+}
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+    alignItems: 'stretch',
+    marginTop: 15,
+  },
+  button: {
+    flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    backgroundColor: '#eee',
+    padding: 10,
   },
-  headline: {
-    fontSize: 30,
-    textAlign: 'center',
-    margin: 10,
+  middleButton: {
+    borderLeftWidth: 1,
+    borderRightWidth: 1,
+    borderColor: '#ccc',
   },
-  valueContainer: {
-    flexDirection: 'row',
-    flexWrap: 'wrap',
-  },
-  valueValue: {
-    width: 200,
-    fontSize: 20
-  },
-  valueName: {
-    width: 50,
-    fontSize: 20,
-    fontWeight: 'bold'
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+  sensor: {
+    marginTop: 15,
+    paddingHorizontal: 10,
   },
 });
